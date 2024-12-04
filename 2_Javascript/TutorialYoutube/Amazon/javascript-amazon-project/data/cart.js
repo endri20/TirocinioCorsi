b88ff1c14ef4f1b311c2a0081a18b1ad0f98a957
Endri Hoxha <hoxhaendri00@gmail.com>
2024-12-04 12:15:12 +0100

    Move addToCart into cart.js

--- 2_Javascript/TutorialYoutube/Amazon/javascript-amazon-project/data/cart.js
+++ 2_Javascript/TutorialYoutube/Amazon/javascript-amazon-project/data/cart.js
@@ -1,2 +1,26 @@
 export const cart = [];
-export const saluto = 'hello';
+
+export function addToCart(productId) {
+  let matchingItem;
+  cart.forEach((CartItem) => {
+    if (productId === CartItem.productId) {
+      matchingItem = CartItem;
+    }
+  });
+  if (matchingItem) {
+    matchingItem.quantity++;
+  } else {
+    cart.push({
+      productId: productId,
+      quantity: 1,
+    });
+  }
+}
+
+export function updateCartQuantity(){
+  let cartQuantity = 0;
+    cart.forEach((CartItem) => {
+      cartQuantity += CartItem.quantity;
+    });
+    document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
+}
