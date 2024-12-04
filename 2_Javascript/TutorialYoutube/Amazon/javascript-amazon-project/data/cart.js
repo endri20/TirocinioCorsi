18747a1c0461580af912a558f4d98507f986a474
Endri Hoxha <hoxhaendri00@gmail.com>
2024-12-04 15:19:55 +0100

    Save cart in localStorage

--- 2_Javascript/TutorialYoutube/Amazon/javascript-amazon-project/data/cart.js
+++ 2_Javascript/TutorialYoutube/Amazon/javascript-amazon-project/data/cart.js
@@ -1,11 +1,21 @@
-export let cart = [{
-productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-quantity: 2,
-},
-{
-  productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
-  quantity:1,
-}];
+export let cart = JSON.parse(localStorage.getItem("cart"));
+
+if (!cart) {
+  cart = [
+    {
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity: 2,
+    },
+    {
+      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      quantity: 1,
+    },
+  ];
+}
+
+function saveToStorage() {
+  localStorage.setItem("cart", JSON.stringify(cart));
+}
 
 export function addToCart(productId) {
   let matchingItem;
@@ -22,24 +32,26 @@ export function addToCart(productId) {
       quantity: 1,
     });
   }
+  saveToStorage();
 }
 
-export function updateCartQuantity(){
+export function updateCartQuantity() {
   let cartQuantity = 0;
-    cart.forEach((CartItem) => {
-      cartQuantity += CartItem.quantity;
-    });
-    document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
+  cart.forEach((CartItem) => {
+    cartQuantity += CartItem.quantity;
+  });
+  document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
 }
 
-export function removeFromCart(productId){
+export function removeFromCart(productId) {
   const newCart = [];
 
-  cart.forEach((cartItem) =>{
-    if(cartItem.productId !== productId){
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
       newCart.push(cartItem);
     }
   });
 
   cart = newCart;
+  saveToStorage();
 }
